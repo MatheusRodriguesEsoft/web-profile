3c97fb83a1151c07dd1ac2556bf880cd622a0775
Matheus Rodrigues <matheus.rodrigues.esoft@gmail.com>
2025-06-26 07:04:54 -0300

    update-26-06-2025

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -14,7 +14,7 @@ export const Header = () => {
     >
       <div className="w-full uppercase max-w-[1440px] flex items-center justify-between px-[20px] md:px-[80px] lg:px-[120px]">
         <Link
-          href={"https://landing-page-opal-one-94.vercel.app/"}
+          href={"https://web-profile-ivory.vercel.app/"}
           className="flex items-center w-12 h-12 animate__animated animate__backInDown"
         >
           <span className="logo">MR.</span>
